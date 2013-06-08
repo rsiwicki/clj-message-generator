0876de8a4e07e7f49266043f3ccf49ae38be1423
robert.siwicki@googlemail.com <rob@Robs-MacBook-Air.local>
2013-06-08 19:12:36 +0100

    initial

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-message-generator
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/clj_message_generator/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-message-generator.core-test
+  (:use clojure.test
+        clj-message-generator.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
